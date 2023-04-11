d2b950951d581bcacb3f2311697c31fa112239b9
Vasco Silva <100089965+reiid00@users.noreply.github.com>
2023-04-11 14:29:11 +0100

    Emotion API working, returns an array with the emotions.

--- UI/emotion_api.py
+++ UI/emotion_api.py
@@ -3,21 +3,25 @@ import torch
 from DistilbertMultiLabel import DistilBertForMultilabelSequenceClassification
 from transformers import AutoTokenizer
 import numpy as np
+import json
 
 model = "Reiid/go-emotional-analysis-distilbert"
 
 app = Flask(__name__)
 
-tokenizer = AutoTokenizer.from_pretrained(model)
-model = DistilBertForMultilabelSequenceClassification.from_pretrained(model).to('cuda')
-threshold = 0.5
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+tokenizer = AutoTokenizer.from_pretrained(model)
+model = DistilBertForMultilabelSequenceClassification.from_pretrained(model).to(device)
+threshold = 0.5
+
+with open('UI\data\i2label.json', 'r') as f:
+    id2label = json.load(f)
 
 @app.route('/predict_emotion', methods=['POST'])
 def predict():
     text = request.json['text']
     binary_preds = predict_label(text)
-    return jsonify({'binary_preds': binary_preds.tolist()})
+    return jsonify({'emotions': binary_preds})
 
 def predict_label(text, threshold=threshold, tokenizer=tokenizer, model=model):
     inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True).to(device)
@@ -25,8 +29,13 @@ def predict_label(text, threshold=threshold, tokenizer=tokenizer, model=model):
     logits = outputs.logits.detach().cpu().numpy()[0]
     probs = softmax(logits)
     probs_scaled = (probs - np.min(probs)) / (np.max(probs) - np.min(probs))
-    binary_preds = np.where(probs_scaled >= threshold, 1, 0)
-    return binary_preds
+    binary_preds = np.where(probs_scaled >= threshold)[0]
+    return binary_preds_to_emotions(binary_preds)
+
+def binary_preds_to_emotions(binary_preds):
+    pred_emotions = [id2label[str(pred_id)]  for pred_id in binary_preds]
+    return pred_emotions
+
 
 def softmax(x):
     e_x = np.exp(x - np.max(x))
